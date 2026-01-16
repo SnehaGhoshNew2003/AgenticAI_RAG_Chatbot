af076e33ea12521d35d76cad5714780a2fe7dc81
SnehaGhoshNew2003 <snehanew2003@gmail.com>
2026-01-16 16:13:37 +0530

    Update vectorstore.py

--- app/vectorstore.py
+++ app/vectorstore.py
@@ -1,29 +1,3 @@
-'''from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.vectorstores import Chroma
-from app.config import CHROMA_DIR, HF_EMBEDDING_MODEL
-
-
-_embeddings = None
-
-
-def get_embeddings():
-    global _embeddings
-    if _embeddings is None:
-        _embeddings = HuggingFaceEmbeddings(
-            model_name=HF_EMBEDDING_MODEL
-        )
-    return _embeddings
-
-
-def get_vectorstore():
-    return Chroma(
-        collection_name="agentic_ai_ebook",
-        persist_directory=CHROMA_DIR,
-        embedding_function=get_embeddings()
-    )
-'''
-
-
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from app.config import DB_PATH, EMBED_MODEL
@@ -41,4 +15,5 @@ def get_vectorstore():
         collection_name="ebook_db",
         persist_directory=DB_PATH,
         embedding_function=get_embeddings()
+
     )
