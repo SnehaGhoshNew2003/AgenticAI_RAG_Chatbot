432a02b1213e92c188383b9ccb63d5f341bf3613
SnehaGhoshNew2003 <snehanew2003@gmail.com>
2026-01-16 16:13:18 +0530

    Update llm.py

--- app/llm.py
+++ app/llm.py
@@ -1,15 +1,3 @@
-'''from langchain_google_genai import ChatGoogleGenerativeAI
-from app.config import LLM_MODEL, GOOGLE_API_KEY
-
-def get_llm():
-    return ChatGoogleGenerativeAI(
-        model=LLM_MODEL,
-        google_api_key=GOOGLE_API_KEY,
-        temperature=2
-    )
-'''
-
-
 from langchain_google_genai import ChatGoogleGenerativeAI
 from app.config import MODEL, API_KEY
 
@@ -23,4 +11,5 @@ def get_llm():
             google_api_key=API_KEY,
             temperature=0.2
         )
+
     return model
